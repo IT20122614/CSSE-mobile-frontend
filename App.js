2b61187a0f56ec84b7fa788025df21462aa32244
IT20122096 <chamathkavvindya@gmail.com>
2022-11-02 04:27:18 +0530

    IT2012096 - header and drawer navigation

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
+import Header from "./app/Retrieve delivered items/Components/common/header";
 import MyDrawer from "./app/Retrieve delivered items/Navigation/AppNavigator";
 export default function App() {
   return (
     <NavigationContainer>
+      <Header/>
       <MyDrawer />
     </NavigationContainer>
   );
